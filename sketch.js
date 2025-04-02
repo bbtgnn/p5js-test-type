1e6ced16a2d9a58e6bd53877286e4f4eaa505d5a
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2025-04-02 18:08:09 +0200

    stuff

--- sketch.js
+++ sketch.js
@@ -1,15 +1,61 @@
+/** @typedef {import("p5").Font} Font */
+
+let diameter = 100;
+
+/** @type {Font} */
+let font;
+
+function preload() {
+  font = loadFont("./assets/InputMonoCondensed-Light.ttf");
+}
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, "webgl");
+  angleMode(DEGREES);
+
+  textFont(font);
+  textSize(100);
 }
 
 function draw() {
-  background(220);
-  const s = 100;
+  background(0);
 
-  rect(0, 0, s, s);
-  rect(0, height - s, s, s);
-  rect(width - s, 0, s, s);
-  rect(width - s, height - s, s, s);
+  orbitControl();
+
+  // directionalLight(255, 0, 0, 1, 1, 0);
+  // ambientLight(80);
+
+  let count = mouseX;
+
+  let angle = 360 / count;
+
+  fill("red");
+
+  // push();
+  // normalMaterial();
+  // sphere(50);
+  // pop();
+
+  for (let i = 0; i < count; i++) {
+    push();
+    rotateY(angle * i);
+    translate(diameter, 0, 0);
+    text("Ciao", 0, 0);
+    // ellipse(0, 0, diameter, diameter);
+    pop();
+  }
+
+  // textSize(100);
+  // for (let i = 0; i < count; i++) {
+  //   push();
+  //   noFill();
+  //   stroke("black");
+  //   // rotateY(angle * i);
+  //   translate(diameter, 0, 0);
+  //   text("Ciao", 0, 0);
+  //   // ellipse(0, 0, diameter, diameter);
+  //   pop();
+  // }
 }
 
 function windowResized() {
