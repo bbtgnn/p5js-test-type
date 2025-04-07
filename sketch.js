5665351c8410f4cdf0a181302b381f3a1ff51cdf
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2025-04-08 00:05:05 +0200

    stuff

--- sketch.js
+++ sketch.js
@@ -14,33 +14,42 @@ function setup() {
   angleMode(DEGREES);
 
   textFont(font);
-  textSize(100);
+  textSize(height / 10);
 }
 
 function draw() {
-  background(0);
+  background("blue");
 
   orbitControl();
 
   // directionalLight(255, 0, 0, 1, 1, 0);
   // ambientLight(80);
 
-  let count = mouseX;
+  let count = 10;
+  let rows = 10;
 
   let angle = 360 / count;
 
-  fill("red");
+  fill("white");
 
   // push();
   // normalMaterial();
   // sphere(50);
   // pop();
 
+  rotateY(-frameCount);
   for (let i = 0; i < count; i++) {
     push();
     rotateY(angle * i);
     translate(diameter, 0, 0);
-    text("Ciao", 0, 0);
+    for (let j = 0; j < rows; j++) {
+      const a = cos(frameCount * 5 + j * 20);
+      const m = map(a, -1, 1, 0, textSize());
+      push();
+      translate(m, textSize() * (j - rows / 2), 0);
+      text("Ciao", 0, 0);
+      pop();
+    }
     // ellipse(0, 0, diameter, diameter);
     pop();
   }
